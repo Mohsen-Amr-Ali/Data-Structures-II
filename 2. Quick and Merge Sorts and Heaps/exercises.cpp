85f422bac51074b340135a0b40a72144e2c7fafb
mohsen <en.mohsenali@gmail.com>
2026-03-13 07:18:34 +0200

    Implemented and tested quickSelect

---
+++ 2. Quick and Merge Sorts and Heaps/exercises.cpp
@@ -0,0 +1,18 @@
+#include "sorts.h"
+#include <vector>
+
+using namespace std;
+
+int quickSelect(vector<int> &arr, int start, int end, int k){
+    int pivot = randomPartition(arr, start, end);
+
+    if (pivot < k-1) { //kth element after pivot, take the right side of the array
+        return quickSelect(arr, pivot + 1, end, k);
+    }
+
+    if (pivot > k-1) { //kth element before pivot, take the left side of the array
+        return quickSelect(arr, start, pivot - 1, k);
+    }
+
+    return arr[pivot]; //basecase
+}
